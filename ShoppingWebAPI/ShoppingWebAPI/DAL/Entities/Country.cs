4d03b39e724bd85cba63ab3f171e4ba513adb26f
Juliana Arroyave Arango <julianaarroyave301009@correo.itm.edu.co>
2023-03-20 15:15:55 -0500

    New DataBaseContext and ConnString cread to configure my new DataBase

---
+++ ShoppingWebAPI/ShoppingWebAPI/DAL/Entities/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingWebAPI.DAL.Entities
+{
+    public class Country : Entity
+    {
+        [Display(Name = "País")]//ASí ES COMO SE VA A MOSTAR POR UI
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]//lONGITUD MAXIMA(NVARCHAR(50))
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")] //NOT NULL
+        public string Name { get; set; }
+
+    }
+}
